73f210ed8f4d4643ef838a5a2756d6bc671bbec7
silvalnk <web4dev.lucas@outlook.com>
2026-06-23 01:31:48 -0300

    chore: 📦 add authentication routes to server

--- backend/src/app/server.rb
+++ backend/src/app/server.rb
@@ -5,6 +5,7 @@ require 'erb'
 require_relative 'initializers/environment'
 require_relative 'docs/swagger_docs'
 require_relative 'routes/api/v1/rooms_routes'
+require_relative 'routes/api/v1/auth_routes'
 
 module App
   class Server < Hanami::API
@@ -26,6 +27,7 @@ module App
       ERB.new(File.read(path)).result(binding)
     end
 
+    App::Routes::API::V1::AuthRoutes.register(self)
     App::Routes::API::V1::RoomsRoutes.register(self)
   end
 end
